e1c6f588af4272863eeb50d729d4d86204637f62
shun-shun123 <shunsuke15vb@gmail.com>
2021-09-22 21:58:21 +0900

    DefaultIfEmptyについて

---
+++ UseCase/DefaultIfEmptyUseCase.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+
+namespace DetailResearchOfLinq
+{
+    public class DefaultIfEmptyUseCase : BaseUseCase
+    {
+        public DefaultIfEmptyUseCase(int size) : base(size)
+        {
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            DefaultIfEmpty01();
+            DefaultIfEmpty02();
+        }
+
+        private void DefaultIfEmpty01()
+        {
+            // シーケンスが値を持っていたらそれをそのまま返す
+            var defaultValue = Users.DefaultIfEmpty();
+            foreach (var val in defaultValue)
+            {
+                LogUtility.Log($"Users.DefaultIfEmpty: {val}");
+            }
+        }
+
+        private void DefaultIfEmpty02()
+        {
+            // シーケンスが値を持っていなかったら、デフォルト値を返す
+            var emptyNumbers = new int[0];
+            var defaultValue = emptyNumbers.DefaultIfEmpty(0);
+            foreach (var val in defaultValue)
+            {
+                LogUtility.Log($"emptyNumbers.DefaultIfEmpty: {val}");
+            }
+        }
+    }
+}
