eff48580031523d1d71808f636fa9567816d4e06
shun-shun123 <shunsuke15vb@gmail.com>
2021-09-22 22:54:06 +0900

    Firstについて

---
+++ UseCase/FirstUseCase.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+
+namespace DetailResearchOfLinq
+{
+    public class FirstUseCase : BaseUseCase
+    {
+        public FirstUseCase(int size) : base(size)
+        {
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            First01();
+            First02();
+            First03();
+        }
+
+        private void First01()
+        {
+            // 単純にシーケンスの一番目の要素を取得する
+            var first = Users.First();
+            LogUtility.Log($"first.User: {first.Name}");
+        }
+
+        private void First02()
+        {
+            // 条件を指定して該当する一番目のものを取得もできる　
+            var firstHit = Users.First(user => user.Age > 1);
+            LogUtility.Log($"first.hit.User: {firstHit.Name}");
+        }
+
+        private void First03()
+        {
+            try
+            {
+                // 該当するものがなければ例外を投げる
+                var firstHit = Users.First(user => user.Age >= 1000);
+            }
+            catch (Exception ex)
+            {
+                LogUtility.Log(ex.Message);
+            }
+        }
+    }
+}
