8abd11e342c35c0aa9ff5916cf30bd9e965c8eea
shun-shun123 <shunsuke15vb@gmail.com>
2021-09-22 21:57:43 +0900

    Averageについて

---
+++ UseCase/AverageUseCase.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+
+namespace DetailResearchOfLinq
+{
+    public class AverageUseCase : BaseUseCase
+    {
+        public AverageUseCase(int size) : base(size)
+        {
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            Average01();
+        }
+
+        private void Average01()
+        {
+            // intだろうが、平均値はdoubleで返ってくる
+            var average = Users.Select(user => user.Age)
+                .Average();
+            LogUtility.Log($"average: {average}");
+
+            var newAverage = Users.Select(user => user.Age)
+                .Average(age => age * 2);
+            LogUtility.Log($"NewAverage: {newAverage}");
+        }
+    }
+}
