4e4294d3a09d454ed3cd730c89022227e7bd8b11
shun-shun123 <shunsuke15vb@gmail.com>
2021-09-22 21:57:29 +0900

    Appendについて

---
+++ UseCase/AppendUseCase.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+
+namespace DetailResearchOfLinq
+{
+    public class AppendUseCase : BaseUseCase
+    {
+        public AppendUseCase(int size) : base(size)
+        {
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            Append01();
+        }
+
+        private void Append01()
+        {
+            // シーケンスの末尾に要素が付け加えられる
+            var users = Users.Append(new User(100, "append user", 40));
+            foreach (var user in users)
+            {
+                LogUtility.Log($"user: {user.Id}");
+            }
+        }
+    }
+}
