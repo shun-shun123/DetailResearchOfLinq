292c7e708374c1913ced434544f542e81ac80e39
shun-shun123 <shunsuke15vb@gmail.com>
2021-09-22 21:57:50 +0900

    Castについて

---
+++ UseCase/CastUseCase.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+
+namespace DetailResearchOfLinq
+{
+    public class CastUseCase : BaseUseCase
+    {
+        public CastUseCase(int size) : base(size)
+        {
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            Cast01();
+        }
+
+        private void Cast01()
+        {
+            // UserをObjectにキャストしたシーケンスを返す
+            var objects = Users.Cast<object>();
+            foreach (var user in objects)
+            {
+                LogUtility.Log($"user.GetType: {user.GetType()}");
+            }
+        }
+    }
+}
