63aaafa53104f5380a3b2108b5d6f74228c9bc43
shun-shun123 <shunsuke15vb@gmail.com>
2021-09-22 21:57:23 +0900

    Anyについて

---
+++ UseCase/AnyUseCase.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+
+namespace DetailResearchOfLinq
+{
+    public class AnyUseCase : BaseUseCase
+    {
+        public AnyUseCase(int size) : base(size)
+        {
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            Any01();
+            Any02();
+        }
+
+        private void Any01()
+        {
+            // 引数なしのAnyは一つでも要素があるかをチェックする
+            var any = Users.Any();
+            LogUtility.Log($"Has.any: {any}");
+
+            // 空の場合はfalseが返る
+            var empty = new int[0];
+            LogUtility.Log($"Empty.Any: {empty.Any()}");
+        }
+
+        private void Any02()
+        {
+            var any = Users.Any(user =>
+            {
+                // 該当するものが見つかるまで操作し続ける
+                LogUtility.LogDetail($"user.Age: {user.Age}");
+                return user.Age == 15;
+            });
+            LogUtility.Log($"any: {any}");
+        }
+    }
+}
