276a3c2113c1786ea47c16b4fd12caac5ba38dde
shun-shun123 <shunsuke15vb@gmail.com>
2021-09-22 21:57:58 +0900

    Concatについて

---
+++ UseCase/ConcatUseCase.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+
+namespace DetailResearchOfLinq
+{
+    public class ConcatUseCase : BaseUseCase
+    {
+        public ConcatUseCase(int size) : base(size)
+        {
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            Concat01();
+        }
+
+        private void Concat01()
+        {
+            // 二つのシーケンスを連結する
+            var doubleUsers = Users.Concat(Users);
+            foreach (var user in doubleUsers)
+            {
+                LogUtility.Log($"user: {user.Name}");
+            }
+        }
+    }
+}
