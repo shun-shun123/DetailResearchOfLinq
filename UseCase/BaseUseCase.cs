c7d8209a7777897a3ceaaac204db9857e44bac36
shun-shun123 <shunsuke15vb@gmail.com>
2021-09-22 22:02:01 +0900

    UseCaseの基盤実装

---
+++ UseCase/BaseUseCase.cs
@@ -0,0 +1,17 @@
+namespace DetailResearchOfLinq
+{
+    public abstract class BaseUseCase
+    {
+        protected User[] Users;
+
+        protected BaseUseCase(int size)
+        {
+            Users = UserFactory.CreateUserArray(size);
+        }
+
+        public virtual void Execute()
+        {
+            LogUtility.Log($"====={GetType().Name}=====");
+        }
+    }
+}
