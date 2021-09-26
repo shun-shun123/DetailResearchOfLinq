d88a8fe65460b10958e00a5d53758c6d30640437
shun-shun123 <shunsuke15vb@gmail.com>
2021-09-26 15:46:01 +0900

    predictを持たないFirstOrDefaultの改良

--- Improve/ImprovedFirstOrDefault.cs
+++ Improve/ImprovedFirstOrDefault.cs
@@ -3,6 +3,33 @@ using System.Collections.Generic;
 
 public static partial class ImprovedLinq
 {
+    public static TSource FirstOrDefault<TSource>(this TSource[] source)
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        if (source.Length == 0) return default;
+        return source[0];
+    }
+
+    public static TSource FirstOrDefault<TSource>(this List<TSource> source)
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        if (source.Count == 0) return default;
+        return source[0];
+    }
+
+    public static KeyValuePair<TKey, TSource> FirstOrDefault<TKey, TSource>(this Dictionary<TKey, TSource> source)
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        if (source.Count == 0) return default;
+        using var enumerator = source.GetEnumerator();
+        if (enumerator.MoveNext())
+        {
+            return enumerator.Current;
+        }
+
+        return default;
+    }
+    
     public static TSource FirstOrDefault<TSource>(this TSource[] source, Func<TSource, bool> predicate)
     {
         if (source == null) throw new ArgumentNullException(nameof(source));
